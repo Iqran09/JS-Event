5c11ca5042d3a20e1d829ba6c1fe6dbdf46aea65
Iqran09 <133879912+Iqran09@users.noreply.github.com>
2023-05-24 02:53:45 -0700

    Add files via upload

---
+++ JS Event/app.js
@@ -0,0 +1,25 @@
+var inp1 = document.getElementById("firstbox");
+var inp2 = document.getElementById("secondbox");
+var inp3 = document.getElementById("thirdbox");
+var inp4 = document.getElementById("fbox");
+var inp5 = document.getElementById("day");
+var inp6 = document.getElementById("month");
+var inp7 = document.getElementById("year");
+var inp8 = document.getElementById("b");
+var inp9 = document.getElementById("c");
+var inp10 = document.getElementById("d");
+
+function getvalue(i){
+    console.log(inp1.value);
+    console.log(inp2.value);
+    console.log(inp3.value);
+    console.log(inp4.value);
+    console.log(inp5.value);
+    console.log(inp6.value);
+    console.log(inp7.value);
+    console.log(inp8.value);
+    console.log(inp9.value);
+    console.log(inp10.value);
+
+
+}
